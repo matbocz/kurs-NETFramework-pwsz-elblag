ec6f128a20926e1be6536248f8ac71369de0ad33
Mateusz Boczarski <matbocz@outlook.com>
2020-01-14 22:13:24 +0100

    Create NETFramework_zad_7

---
+++ NETFramework_zad_7/PaintApp/Form1.Designer.cs
@@ -0,0 +1,178 @@
+﻿namespace PaintApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.labelPath = new System.Windows.Forms.Label();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.buttonOpen = new System.Windows.Forms.Button();
+            this.buttonZapisz = new System.Windows.Forms.Button();
+            this.buttonCzysc = new System.Windows.Forms.Button();
+            this.buttonKolor = new System.Windows.Forms.Button();
+            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.buttonDrukuj = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.Window;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 33);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(639, 313);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseDown);
+            this.pictureBox1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseMove);
+            // 
+            // labelPath
+            // 
+            this.labelPath.AutoSize = true;
+            this.labelPath.Location = new System.Drawing.Point(13, 13);
+            this.labelPath.Name = "labelPath";
+            this.labelPath.Size = new System.Drawing.Size(0, 17);
+            this.labelPath.TabIndex = 1;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // buttonOpen
+            // 
+            this.buttonOpen.Location = new System.Drawing.Point(657, 33);
+            this.buttonOpen.Name = "buttonOpen";
+            this.buttonOpen.Size = new System.Drawing.Size(131, 23);
+            this.buttonOpen.TabIndex = 2;
+            this.buttonOpen.Text = "Otwórz";
+            this.buttonOpen.UseVisualStyleBackColor = true;
+            this.buttonOpen.Click += new System.EventHandler(this.buttonOpen_Click);
+            // 
+            // buttonZapisz
+            // 
+            this.buttonZapisz.Location = new System.Drawing.Point(657, 63);
+            this.buttonZapisz.Name = "buttonZapisz";
+            this.buttonZapisz.Size = new System.Drawing.Size(131, 23);
+            this.buttonZapisz.TabIndex = 3;
+            this.buttonZapisz.Text = "Zapisz";
+            this.buttonZapisz.UseVisualStyleBackColor = true;
+            this.buttonZapisz.Click += new System.EventHandler(this.buttonZapisz_Click);
+            // 
+            // buttonCzysc
+            // 
+            this.buttonCzysc.Location = new System.Drawing.Point(657, 133);
+            this.buttonCzysc.Name = "buttonCzysc";
+            this.buttonCzysc.Size = new System.Drawing.Size(131, 23);
+            this.buttonCzysc.TabIndex = 4;
+            this.buttonCzysc.Text = "Czyść";
+            this.buttonCzysc.UseVisualStyleBackColor = true;
+            this.buttonCzysc.Click += new System.EventHandler(this.buttonCzysc_Click);
+            // 
+            // buttonKolor
+            // 
+            this.buttonKolor.Location = new System.Drawing.Point(657, 322);
+            this.buttonKolor.Name = "buttonKolor";
+            this.buttonKolor.Size = new System.Drawing.Size(131, 23);
+            this.buttonKolor.TabIndex = 5;
+            this.buttonKolor.Text = "Kolor";
+            this.buttonKolor.UseVisualStyleBackColor = true;
+            this.buttonKolor.Click += new System.EventHandler(this.buttonKolor_Click);
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(657, 352);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(131, 22);
+            this.numericUpDown1.TabIndex = 6;
+            this.numericUpDown1.ValueChanged += new System.EventHandler(this.numericUpDown1_ValueChanged);
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(12, 353);
+            this.trackBar1.Maximum = 100;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(639, 56);
+            this.trackBar1.TabIndex = 7;
+            this.trackBar1.Scroll += new System.EventHandler(this.trackBar1_Scroll);
+            // 
+            // buttonDrukuj
+            // 
+            this.buttonDrukuj.Location = new System.Drawing.Point(658, 163);
+            this.buttonDrukuj.Name = "buttonDrukuj";
+            this.buttonDrukuj.Size = new System.Drawing.Size(130, 23);
+            this.buttonDrukuj.TabIndex = 8;
+            this.buttonDrukuj.Text = "Drukuj";
+            this.buttonDrukuj.UseVisualStyleBackColor = true;
+            this.buttonDrukuj.Click += new System.EventHandler(this.buttonDrukuj_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.buttonDrukuj);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.buttonKolor);
+            this.Controls.Add(this.buttonCzysc);
+            this.Controls.Add(this.buttonZapisz);
+            this.Controls.Add(this.buttonOpen);
+            this.Controls.Add(this.labelPath);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label labelPath;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button buttonOpen;
+        private System.Windows.Forms.Button buttonZapisz;
+        private System.Windows.Forms.Button buttonCzysc;
+        private System.Windows.Forms.Button buttonKolor;
+        private System.Windows.Forms.ColorDialog colorDialog1;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.Button buttonDrukuj;
+    }
+}
+
---
+++ NETFramework_zad_7/PaintApp/Form1.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Printing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PaintApp
+{
+    public partial class Form1 : Form
+    {
+        private Graphics g;
+        private Point point1Begin = Point.Empty;
+        private Pen pen1;
+
+        public Form1()
+        {
+            InitializeComponent();
+            pictureBox1.Image = new Bitmap(639, 313);
+            g = Graphics.FromImage(pictureBox1.Image);
+            pen1 = new Pen(Color.FromArgb(255, 0, 0, 0));
+        }
+
+        private void pictureBox1_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+                point1Begin = e.Location;
+        }
+
+        private void pictureBox1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                g.DrawLine(pen1, point1Begin, e.Location);
+                point1Begin = e.Location;
+                pictureBox1.Refresh();
+            }
+        }
+
+        private void buttonOpen_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.ShowDialog();
+            if (openFileDialog1.FileName != "")
+            {
+                pictureBox1.Load(openFileDialog1.FileName);
+                g = Graphics.FromImage(pictureBox1.Image);
+                this.labelPath.Text = openFileDialog1.FileName;
+            }
+        }
+
+        private void buttonCzysc_Click(object sender, EventArgs e)
+        {
+            g.Clear(Color.White);
+            pictureBox1.Refresh();
+            this.labelPath.Text = "";
+        }
+
+        private void buttonKolor_Click(object sender, EventArgs e)
+        {
+            colorDialog1.ShowDialog();
+            buttonKolor.BackColor = colorDialog1.Color;
+            pen1.Color = colorDialog1.Color;
+        }
+
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+            pen1.Width = float.Parse(this.numericUpDown1.Value.ToString());
+            trackBar1.Value = int.Parse(this.numericUpDown1.Value.ToString());
+        }
+
+        private void trackBar1_Scroll(object sender, EventArgs e)
+        {
+            pen1.Width = float.Parse(this.trackBar1.Value.ToString());
+            numericUpDown1.Value = this.trackBar1.Value;
+        }
+
+        private void buttonZapisz_Click(object sender, EventArgs e)
+        {
+            saveFileDialog1.ShowDialog();
+            pictureBox1.Image.Save(saveFileDialog1.FileName);
+        }
+
+        private void buttonDrukuj_Click(object sender, EventArgs e)
+        {
+            PrintDialog pd = new PrintDialog();
+            PrintDocument doc = new PrintDocument();
+            doc.PrintPage += Doc_printPage;
+            pd.Document = doc;
+            if (pd.ShowDialog() == DialogResult.OK)
+            {
+                doc.Print();
+            }
+        }
+
+        private void Doc_printPage(object sender, PrintPageEventArgs e)
+        {
+            Bitmap bm = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            pictureBox1.DrawToBitmap(bm, new Rectangle(0, 0, pictureBox1.Width, pictureBox1.Height));
+            e.Graphics.DrawImage(bm, 0, 0);
+            bm.Dispose();
+        }
+    }
+}
